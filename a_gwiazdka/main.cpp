67f9f77f1fa3a8fd33a50daaeab66e260516b1a8
lakiert <73653530+lakiert@users.noreply.github.com>
2022-10-22 02:15:27 +0200

    Update main.cpp

--- a_gwiazdka/main.cpp
+++ a_gwiazdka/main.cpp
@@ -26,43 +26,54 @@ return g;
 
 
 
-void mozliwe_do_odwiedzenia(int map[wym2][wym1], int otwarta[wym2][wym1], int parent[wym2][wym1], int x, int y)
+void mozliwe_do_odwiedzenia(int map[wym2][wym1], int otwarta[wym2][wym1], int parent[wym2][wym1], int &x, int &y, int zamknieta[wym2][wym1], int &finish, int cel_x, int cel_y)
 {
 	
-if ( (map[x+1][y] == 0) && ((x+1) <= wym2) )
+if ( ((x+1 == cel_x)&&(y == cel_y)) || ((x==cel_x)&&(y-1==cel_y)) || ((x-1==cel_x)&&(y==cel_y)) || ((x==cel_x)&&(y+1==cel_y)) )
+{
+	map[cel_x][cel_y] = 3;
+	finish = 1;
+}
+else
+{
+		
+	
+if ( (map[x+1][y] == 0) && (zamknieta[x+1][y] == 0) && ((x+1) <= wym2) )
 	{
-		cout<<"dol istnieje\n";
+//		cout<<"dol istnieje\n";
 		otwarta[x+1][y] = 0;
 		parent[x+1][y] = 0;
 	}	
 	
-if ( (map[x][y-1] == 0) && ((y-1) > 0) )
+if ( (map[x][y-1] == 0) && (zamknieta[x][y-1] == 0) && ((y-1) > 0) )
 	{
-		cout<<"lewo istnieje\n";
+//		cout<<"lewo istnieje\n";
 		otwarta[x][y-1] = 1;
 		parent[x][y-1] = 1;
 	}
 	
-if ( (map[x-1][y] == 0) && ((x-1) > 0) )
+if ( (map[x-1][y] == 0) && (zamknieta[x-1][y] == 0) && ((x-1) > 0) )
 	{
-		cout<<"gora istnieje\n";
+//		cout<<"gora istnieje\n";
 		otwarta[x-1][y] = 2;
 		parent[x-1][y] = 2;
 	}	
 	
-if ( (map[x][y+1] == 0) && ((y+1) <= wym1) ) 
+if ( (map[x][y+1] == 0) && (zamknieta[x][y+1] == 0) && ((y+1) <= wym1) ) 
 	{
-		cout<<"prawo istnieje\n";
+//		cout<<"prawo istnieje\n";
 		otwarta[x][y+1] = 3;
 		parent[x][y+1] = 3;
 	}
+
+}
 }	
 	
 	
-void oblicz_koszta(int otwarta[wym2][wym1], float koszta[wym2][wym1], int start_x,int start_y,int cel_x,int cel_y)
+void oblicz_koszta(int otwarta[wym2][wym1], float koszta[wym2][wym1], int &roboczy_x,int &roboczy_y,int cel_x,int cel_y)
 {
 	
-	float h = funkcja_h(start_x,start_y,cel_x,cel_y);
+	float h = funkcja_h(roboczy_x,roboczy_y,cel_x,cel_y);
 	
 	for(int i=1;i<wym2+1;i++)
  	{
@@ -71,8 +82,8 @@ void oblicz_koszta(int otwarta[wym2][wym1], float koszta[wym2][wym1], int start_
     		if (otwarta[i][j] != 5)
     		{
     			
-    		cout<<endl<<i<<j<<endl<<endl;
-    		float g = funkcja_g(start_x,start_y,i,j);
+//    		cout<<endl<<i<<j<<endl<<endl;
+    		float g = funkcja_g(roboczy_x,roboczy_y,i,j);
     		float f = h + g;
     		koszta[i][j] = f;
     			
@@ -80,6 +91,8 @@ void oblicz_koszta(int otwarta[wym2][wym1], float koszta[wym2][wym1], int start_
 			}
    		}
  	}
+
+
 	
 }
 	
@@ -98,7 +111,7 @@ void wyczysc_otwarta(int otwarta[wym2][wym1])
 }
 
 
-void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwarta[wym2][wym1], int parent[wym2][wym1], float temp[4])
+void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwarta[wym2][wym1], int parent[wym2][wym1], int zamknieta[wym2][wym1], float temp[4], int &roboczy_x, int &roboczy_y)
 {
 
 	int k = 0;
@@ -119,13 +132,13 @@ void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwa
 
 	//wyswietl temp
 
-  		for(int j=0;j<4;j++)
-  		{
-    		cout<<" "<<temp[j];
-   		}
+//  		for(int j=0;j<4;j++)
+//  		{
+//    		cout<<" "<<temp[j];
+//   		}
    		
 	mini = min(min(min(temp[0],temp[1]),temp[2]),temp[3]);
-	cout<<endl<<endl<<"mini: "<<mini;
+//	cout<<endl<<endl<<"mini: "<<mini;
 	
 
  	int licznik = 0;
@@ -151,7 +164,10 @@ void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwa
            
             if (koszta[i][j] == mini)
                {
-                 map[i][j] = 3;              
+                 map[i][j] = 3;
+                 zamknieta[i][j] = map[i][j];
+				 roboczy_x = i;
+				 roboczy_y = j;           
                }             
            }
        }
@@ -179,9 +195,9 @@ void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwa
        }
        
        
-       cout<<endl<<endl<<temp2[0]<<endl<<temp2[1];
+//       cout<<endl<<endl<<temp2[0]<<endl<<temp2[1];
        maxi = max(max(max(temp2[0],temp2[1]),temp2[2]),temp2[3]);
-       cout<<endl<<endl<<maxi;
+//       cout<<endl<<endl<<maxi;
        
        
        for (int i=1;i<wym2+1;i++)
@@ -191,7 +207,10 @@ void wybierz_po_kosztach(int map[wym2][wym1], float koszta[wym2][wym1], int otwa
            
             if (otwarta[i][j] == maxi)
                {
-                map[i][j] = 3;   
+                map[i][j] = 3;
+                zamknieta[i][j] = map[i][j];
+                roboczy_x = i;
+				roboczy_y = j; 
                }             
            }
        }
@@ -221,24 +240,28 @@ int start_x = 1;
 int start_y = 1;
 int cel_x = 3;
 int cel_y = 3;
-int& start = map[start_x][start_y];
-int& cel = map[cel_x][cel_y];
+int start = map[start_x][start_y];
+int cel = map[cel_x][cel_y];
+
+int roboczy_x = start_x;
+int *wsk_x = &roboczy_x;
+
+int roboczy_y = start_y;
+int *wsk_y = &roboczy_y;
+
+int finish = 0;
+int *wsk_finish = &finish;
 
 
 int parent[wym2][wym1];
-//double f[wym2][wym1];
-//int g[wym2][wym1];
-//double h[wym2][wym1];
-
 int otwarta[wym2][wym1];
 int zamknieta[wym2][wym1];
-zamknieta[0][0] = start;
 float koszta[wym2][wym1];
 
 
 
 
-//teraz deklarujemy dynamicznie tablice do, której wczytamyu nasz plik,
+//teraz deklarujemy dynamicznie tablice do, której wczytamy nasz plik,
 int rows = wym2+1;
 double **G;
 G = new double*[rows];
@@ -268,10 +291,7 @@ for(int i=1;i<wym2+1;i++)
    }
  }
  
- map[start_x][start_y] = 3;
- map[cel_x][cel_y] = 3;
- 
- //wypelnianie listy otwartej, parent oraz koszta
+ //wypelnianie listy otwartej,zamknietej, parent oraz koszta
 for(int i=1;i<wym2+1;i++)
  {
   for(int j=1;j<wym1+1;j++)
@@ -279,12 +299,16 @@ for(int i=1;i<wym2+1;i++)
        	otwarta[i][j] = 5;
     	parent[i][j] = 5;
     	koszta[i][j] = 0;
+    	zamknieta[i][j] = 0;
    }
  }
+ zamknieta[start_x][start_y] = 3;
+ parent[start_x][start_y] = 8;
 
 
 
-cout<<"\nWypisujemy na ekran MAPA\n\n";
+
+cout<<"\nMAPA: \n\n";
 for(int i=1;i<wym2+1;i++)
  {
   for(int j=1;j<wym1+1;j++)
@@ -292,6 +316,11 @@ for(int i=1;i<wym2+1;i++)
     cout<<" "<<map[i][j];
    }cout<<"\n";
  }
+
+cout<<endl<<endl;
+cout<<"start: ["<<start_x<<", "<<start_y<<"]"<<endl;
+cout<<"cel: ["<<cel_x<<", "<<cel_y<<"]"<<endl;
+ 
    
 //na koniec czyścimy pamięć po naszej tablicy
 for(int i=0;i<wym2+1;i++)
@@ -302,64 +331,25 @@ delete[] G;//zwalniamy tablice wskaznikow do wierszy
 
 
 
-mozliwe_do_odwiedzenia(map, otwarta, parent, 1, 1);
+////////////////////////ryneczek//////////////////////////////////////
 
-cout<<endl<<endl<<"PARENT:\n";
 
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<parent[i][j];
-   }cout<<"\n";
- }
+map[start_x][start_y]=3;
 
-cout<<endl<<endl<<"OTWARTA:\n";
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<otwarta[i][j];
-   }cout<<"\n";
- }
- 
- 
-// wyczysc_otwarta(otwarta);
-// 
-// cout<<endl<<endl;
-//for(int i=1;i<wym2+1;i++)
-// {
-//  for(int j=1;j<wym1+1;j++)
-//   {
-//    cout<<" "<<otwarta[i][j];
-//   }cout<<"\n";
-// }
- 
- 
- 
- cout<<endl<<endl<<"KOSZTA:\n";
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<koszta[i][j];
-   }cout<<"\n";
- }
- 
- 
-oblicz_koszta(otwarta,koszta,start_x,start_y,cel_x,cel_y);
+	
+	
+while(finish == 0)
+{
 
-cout<<endl<<endl<<"KOSZTA:\n";
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<koszta[i][j];
-   }cout<<"\n";
- }
-//////////
-cout<<endl<<endl<<"MAPA:\n";
+mozliwe_do_odwiedzenia(map, otwarta, parent, roboczy_x, roboczy_y,zamknieta,finish,cel_x,cel_y);
+oblicz_koszta(otwarta,koszta,roboczy_x,roboczy_y,cel_x,cel_y);
+wybierz_po_kosztach(map,koszta,otwarta,parent,zamknieta,temp,roboczy_x,roboczy_y);
+wyczysc_otwarta(otwarta);
 
+}
+
+
+cout<<endl<<endl<<"TRASA: "<<endl<<endl;
 for(int i=1;i<wym2+1;i++)
  {
   for(int j=1;j<wym1+1;j++)
@@ -367,24 +357,6 @@ for(int i=1;i<wym2+1;i++)
     cout<<" "<<map[i][j];
    }cout<<"\n";
  }
-/////////
-cout<<endl<<endl;
-wybierz_po_kosztach(map,koszta,otwarta,parent,temp);
-
-cout<<endl<<endl<<"MAPA:\n";
-
-for(int i=1;i<wym2+1;i++)
- {
-  for(int j=1;j<wym1+1;j++)
-   {
-    cout<<" "<<map[i][j];
-   }cout<<"\n";
- }
-
-
-cout<<map[3][3];
-
-
 
 
 
